9d6949fd6d8b63a92fe57bd34137afa3e991ad0e
mohammadali27 <amohammad6627@gmail.com>
2026-04-17 22:36:21 +0600

    add tost

--- src/component/CallButton/InstalCallButton.jsx
+++ src/component/CallButton/InstalCallButton.jsx
@@ -4,8 +4,10 @@ import { useContext } from "react";
 import { FiPhoneCall } from "react-icons/fi";
 import { IoVideocamOutline } from "react-icons/io5";
 import { MdTextsms } from "react-icons/md";
+import { toast, ToastContainer } from "react-toastify";
 
 const InstalCallButton = ({ app }) => {
+  //  const notify = (type) => toast("call");
   const { installedApp, setInstalledApp, timeline, setTimeline } =
     useContext(InstallAppsContext);
   const addToTimeline = (type) => {
@@ -18,21 +20,26 @@ const InstalCallButton = ({ app }) => {
 
     setTimeline([newEvent, ...timeline]);
     setInstalledApp([...installedApp, app]);
+    toast.success(`${type} started with ${app.name}`);
   };
 
   return (
     <div className="flex items-center shadow-sm gap-5">
       <button onClick={() => addToTimeline("Call")} className="btn">
         <FiPhoneCall /> Call
+        
       </button>
 
       <button onClick={() => addToTimeline("Text")} className="btn">
         <MdTextsms /> Text
+         
       </button>
 
       <button onClick={() => addToTimeline("Video")} className="btn">
         <IoVideocamOutline /> Video
+       
       </button>
+      <ToastContainer/>
     </div>
   );
 };
